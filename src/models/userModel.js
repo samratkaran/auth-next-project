390e3f0176f386f698ac700381794a937647d8bb
Karan samrat <karansamrat05039@gmail.com>
2024-04-04 12:42:59 +0530

    added user schema

---
+++ src/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  usernam: {
+    type: String,
+    required: [true, "please provide a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "please provide a email"],
+    unique: true,
+  },
+  usernam: {
+    password: String,
+    required: [true, "please provide a password"],
+  },
+  isVerified: {
+    password: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    password: Boolean,
+    default: false,
+  },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+
+});
